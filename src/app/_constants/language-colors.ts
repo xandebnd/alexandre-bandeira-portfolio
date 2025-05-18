3416fca0f0289cb8ce147d783c3f01e6f240a353
Alexandre Bandeira <xandebnd@hotmail.com>
2025-05-18 11:40:08 -0300

    refactor: add constant language-colors

---
+++ src/app/_constants/language-colors.ts
@@ -0,0 +1,8 @@
+export const languageColors: { [key: string]: string } = {
+	TypeScript: "text-blue-500 border-blue-500",
+	JavaScript: "text-yellow-400 border-yellow-400",
+	HTML: "text-orange-500 border-orange-500",
+	CSS: "text-blue-300 border-blue-300",
+	Python: "text-green-500 border-green-500",
+	Default: "text-gray-400 border-gray-400",
+};
