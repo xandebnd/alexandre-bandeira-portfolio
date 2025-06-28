c735b920438eb83af5340267923927685076787f
Alexandre Bandeira <xandebnd@hotmail.com>
2025-06-28 16:28:55 -0300

    chore: update dependencies and improve component structure

--- src/app/_components/projects.tsx
+++ src/app/_components/projects.tsx
@@ -1,36 +1,38 @@
+"use client";
+
 import ProjectItem from "@/app/_components/project-item";
 import type { Project } from "@/types/Project";
 import { useEffect, useState } from "react";
 import Section from "./section";
 
 const Projects = () => {
-	const [repos, setRepos] = useState<Project[]>([]);
+  const [repos, setRepos] = useState<Project[]>([]);
 
-	useEffect(() => {
-		const loadRepos = async () => {
-			const res = await fetch("https://api.github.com/users/xandebnd/repos");
+  useEffect(() => {
+    const loadRepos = async () => {
+      const res = await fetch("https://api.github.com/users/xandebnd/repos");
 
-			const data = await res.json();
+      const data = await res.json();
 
-			setRepos(data);
-		};
+      setRepos(data);
+    };
 
-		loadRepos();
-	}, []);
+    loadRepos();
+  }, []);
 
-	return (
-		<Section id="projetos" data-aos="fade-up" data-aos-duration="1000">
-			<h2 className="mb-8 text-center text-4xl font-bold">Projetos</h2>
-			<div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-				{repos
-					.filter(({ description }) => description)
-					.slice(0, 6)
-					.map((project) => (
-						<ProjectItem project={project} key={project.id} />
-					))}
-			</div>
-		</Section>
-	);
+  return (
+    <Section id="projetos" data-aos="fade-up" data-aos-duration="1000">
+      <h2 className="mb-8 text-center text-4xl font-bold">Projetos</h2>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {repos
+          .filter(({ description }) => description)
+          .slice(0, 6)
+          .map((project) => (
+            <ProjectItem project={project} key={project.id} />
+          ))}
+      </div>
+    </Section>
+  );
 };
 
 export default Projects;
